5f8800cfe93d120bc8309a2857b92cd73f67bba8
Mazzya <yahyamazouarenna@gmail.com>
2024-10-09 22:41:52 +0200

    added error messages

--- qubipy/exceptions.py
+++ qubipy/exceptions.py
@@ -3,3 +3,16 @@ exceptions.py
 This file defines custom exceptions specific to the Qubic API,
 allowing for clearer handling of errors and failure messages.
 """
+
+class QubiPy_Exceptions(Exception):
+    """Custom exceptions for the class"""
+
+    INVALID_TICK_ERROR = 'You need to enter a valid tick number'
+
+    INVALID_ADDRESS_ID = 'You need to enter a valid address ID.'
+
+    INVALID_TX_ID = 'You need to enter a valid tx ID'
+
+    INVALID_START_TICK_AND_END_TICK = 'You need to enter a valid starting tick and a valid ending tick'
+    
+    INVALID_EPOCH = 'You need to enter a valid epoch'
