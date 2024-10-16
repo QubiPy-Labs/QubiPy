7e9f0e324cd669cd34bd0cc605dc61636be0e296
Mazzya <yahyamazouarenna@gmail.com>
2024-10-16 16:37:53 +0200

    added quottery methods

--- qubipy/core/core_client.py
+++ qubipy/core/core_client.py
@@ -45,7 +45,7 @@ class QubiPy_Core:
             data = response.json()
             return data
         except requests.RequestException as E:
-            raise QubiPy_Exceptions(f'Error when getting computors: {str(E)}') from None
+            raise QubiPy_Exceptions(f'Error when getting entity info: {str(E)}') from None
 
     
     def get_tick_data(self, tick: Optional[int] = None) -> Dict[str, Any]:
@@ -64,7 +64,7 @@ class QubiPy_Core:
             data = response.json()
             return data
         except requests.RequestException as E:
-            raise QubiPy_Exceptions(f'Error when getting computors: {str(E)}') from None
+            raise QubiPy_Exceptions(f'Error when getting tick data: {str(E)}') from None
     
     
     def get_tick_info(self) -> Dict[str, Any]:
@@ -126,4 +126,82 @@ class QubiPy_Core:
             data = response.json()
             return data
         except requests.RequestException as E:
-            raise QubiPy_Exceptions(f'Error when getting tick transactions: {str(E)}') from None
+            raise QubiPy_Exceptions(f'Error when getting tick transaction status: {str(E)}') from None
+        
+    """ QUOTTERY SERVICES """
+
+    def get_active_bets(self) -> Dict[str, Any]:
+
+        try:
+            response = requests.get(f'{self.core_url}{ACTIVE_BETS}', headers=HEADERS, timeout=self.timeout)
+            response.raise_for_status()  # Raise an exception for bad HTTP status codes
+            data = response.json()
+            return data
+        except requests.RequestException as E:
+            raise QubiPy_Exceptions(f'Error when getting active bets: {str(E)}') from None
+    
+    def get_active_bets_by_creator(self, creator_id: Optional[str] = None) -> Dict[str, Any]:
+
+        if not creator_id:
+            raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_ADDRESS_ID)
+        
+        payload = {
+            'creatorId': creator_id
+        }
+
+
+        try:
+            response = requests.get(f'{self.core_url}{ACTIVE_BETS_BY_CREATOR}', headers=HEADERS, params=payload, timeout=self.timeout)
+            response.raise_for_status()  # Raise an exception for bad HTTP status codes
+            data = response.json()
+            return data
+        except requests.RequestException as E:
+            raise QubiPy_Exceptions(f'Error when getting active bets by creator: {str(E)}') from None
+    
+    def get_basic_info(self) -> Dict[str, Any]:
+
+        try:
+            response = requests.get(f'{self.core_url}{BASIC_INFO}', headers=HEADERS, timeout=self.timeout)
+            response.raise_for_status()  # Raise an exception for bad HTTP status codes
+            data = response.json()
+            return data
+        except requests.RequestException as E:
+            raise QubiPy_Exceptions(f'Error when getting computors: {str(E)}') from None
+    
+    def get_bet_info(self, bet_id: Optional[int] = None) -> Dict[str, Any]:
+
+        if not bet_id:
+            raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_BET_ID)
+        
+        payload = {
+            'betId': bet_id
+        }
+
+
+        try:
+            response = requests.get(f'{self.core_url}{BET_INFO}', headers=HEADERS, params=payload, timeout=self.timeout)
+            response.raise_for_status()  # Raise an exception for bad HTTP status codes
+            data = response.json()
+            return data
+        except requests.RequestException as E:
+            raise QubiPy_Exceptions(f'Error when getting bet info by id: {str(E)}') from None
+        
+    def get_bettors_by_bet_options(self, bet_id: Optional[int] = None, bet_option: Optional[int] = None) -> Dict[str, Any]:
+
+        if not bet_id or not bet_option:
+            raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_BET_OPTIONS)
+        
+        payload = {
+            'betId': bet_id,
+            'betOption': bet_option
+        }
+
+
+        try:
+            response = requests.get(f'{self.core_url}{BETTORS_BY_BET_OPTIONS}', headers=HEADERS, params=payload, timeout=self.timeout)
+            response.raise_for_status()  # Raise an exception for bad HTTP status codes
+            data = response.json()
+            return data
+        except requests.RequestException as E:
+            raise QubiPy_Exceptions(f'Error when getting bet info by id: {str(E)}') from None
+    
