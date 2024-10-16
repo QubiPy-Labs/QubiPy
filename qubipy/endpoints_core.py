62b29d3584165754304db7dacdf22a6c5432df0d
Mazzya <yahyamazouarenna@gmail.com>
2024-10-16 13:51:15 +0200

    added new core endpoints

---
+++ qubipy/endpoints_core.py
@@ -0,0 +1,12 @@
+"""
+endpoints_core.py
+Defines the endpoints used by the Qubic CORE API.
+This facilitates the centralization of routes and their possible future update.
+"""
+
+COMPUTORS = '/core/getComputors'
+
+ENTITY_INFO = '/core/getEntityInfo'
+
+TICK_DATA = '/core/getTickData'
+
