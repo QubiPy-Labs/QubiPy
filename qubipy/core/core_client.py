6126d0afd62d81d0729d248b1a81e87cf2ed84dc
Mazzya <yahyamazouarenna@gmail.com>
2024-10-16 16:14:04 +0200

    added core methods

--- qubipy/core/core_client.py
+++ qubipy/core/core_client.py
@@ -48,7 +48,11 @@ class QubiPy_Core:
             raise QubiPy_Exceptions(f'Error when getting computors: {str(E)}') from None
 
     
-    def get_tick_data(self, tick):
+    def get_tick_data(self, tick: Optional[int] = None):
+
+        if not tick:
+            raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_TICK_ERROR)
+
 
         payload = {
             "tick": tick
@@ -61,5 +65,65 @@ class QubiPy_Core:
             return data
         except requests.RequestException as E:
             raise QubiPy_Exceptions(f'Error when getting computors: {str(E)}') from None
-
     
+    
+    def get_tick_info(self):
+
+        try:
+            response = requests.get(f'{self.core_url}{TICK_INFO}', headers=HEADERS, timeout=self.timeout)
+            response.raise_for_status()  # Raise an exception for bad HTTP status codes
+            data = response.json()
+            return data
+        except requests.RequestException as E:
+            raise QubiPy_Exceptions(f'Error when getting tick info: {str(E)}') from None
+    
+    def get_tick_quorum_vote(self, tick: Optional[int] = None):
+
+        if not tick:
+            raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_TICK_ERROR)
+        
+        payload = {
+            'tick': tick
+        }
+
+        try:
+            response = requests.post(f'{self.core_url}{TICK_QUORUM_VOTE}', headers=HEADERS, json=payload, timeout=self.timeout)
+            response.raise_for_status()  # Raise an exception for bad HTTP status codes
+            data = response.json()
+            return data
+        except requests.RequestException as E:
+            raise QubiPy_Exceptions(f'Error when getting tick quorum vote: {str(E)}') from None
+    
+    def get_tick_transactions(self, tick: Optional[int] = None):
+
+        if not tick:
+            raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_TICK_ERROR)
+        
+        payload = {
+            'tick': tick
+        }
+
+        try:
+            response = requests.post(f'{self.core_url}{TICK_TRANSACTIONS}', headers=HEADERS, json=payload, timeout=self.timeout)
+            response.raise_for_status()  # Raise an exception for bad HTTP status codes
+            data = response.json()
+            return data.get('transactions', {})
+        except requests.RequestException as E:
+            raise QubiPy_Exceptions(f'Error when getting tick transactions: {str(E)}') from None
+        
+    def get_tick_transactions_status(self, tick: Optional[int] = None):
+
+        if not tick:
+            raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_TICK_ERROR)
+        
+        payload = {
+            'tick': tick
+        }
+
+        try:
+            response = requests.post(f'{self.core_url}{TICK_TRANSACTION_STATUS}', headers=HEADERS, json=payload, timeout=self.timeout)
+            response.raise_for_status()  # Raise an exception for bad HTTP status codes
+            data = response.json()
+            return data
+        except requests.RequestException as E:
+            raise QubiPy_Exceptions(f'Error when getting tick transactions: {str(E)}') from None
