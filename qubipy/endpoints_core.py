cf9f2cf76e35dba6c9645ecd828916dcb57ea87b
Mazzya <yahyamazouarenna@gmail.com>
2024-10-16 14:01:35 +0200

    added quottery service endpoints

--- qubipy/endpoints_core.py
+++ qubipy/endpoints_core.py
@@ -10,3 +10,20 @@ ENTITY_INFO = '/core/getEntityInfo'
 
 TICK_DATA = '/core/getTickData'
 
+TICK_INFO = '/core/getTickInfo'
+
+TICK_QUORUM_VOTE = '/core/getTickQuorumVote'
+
+TICK_TRANSACTIONS = '/core/getTickTransactions'
+
+# QUOTTERY SERVICE
+
+ACTIVE_BETS = '/quottery/getActiveBets'
+
+ACTIVE_BETS_BY_CREATOR = '/quottery/getActiveBetsByCreator'
+
+BASIC_INFO = '/quottery/getBasicInfo'
+
+BET_INFO = '/quottery/getBetInfo'
+
+BETTORS_BY_BET_OPTIONS = '/quottery/getBettorsByBetOption'
