19e5be460ae35492eaf7840b40989e7f5512a23e
Mazzya <yahyamazouarenna@gmail.com>
2024-10-16 16:45:58 +0200

    added docstrings

--- qubipy/core/core_client.py
+++ qubipy/core/core_client.py
@@ -22,6 +22,17 @@ class QubiPy_Core:
     
     def get_computors(self) -> Dict[str, Any]:
 
+        """
+        Retrieves the list of computors from the core server.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the computors data from the server. 
+                            If no data is retrieved, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If there is an issue with the API request, such as a network error, invalid response, or timeout.
+        """
+
         try:
             response = requests.get(f'{self.core_url}{COMPUTORS}', timeout=self.timeout)
             response.raise_for_status()  # Raise an exception for bad HTTP status codes
@@ -31,6 +42,20 @@ class QubiPy_Core:
             raise QubiPy_Exceptions(f'Error when getting computors: {str(E)}') from None
     
     def get_entity_info(self, id: Optional[str] = None) -> Dict[str, Any]:
+
+        """
+        Retrieves information about a specific entity from the core server based on the provided entity ID.
+
+        Args:
+            id (Optional[str]): The ID of the entity to retrieve information for. Must be a valid string.
+                                If no ID is provided, an exception is raised.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the entity's information. If no data is available, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If the provided ID is invalid or not provided, or if there is an issue with the API request (e.g., network error, invalid response, or timeout).
+        """
         
         if not id:
             raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_ADDRESS_ID)
@@ -49,6 +74,21 @@ class QubiPy_Core:
 
     
     def get_tick_data(self, tick: Optional[int] = None) -> Dict[str, Any]:
+
+        """
+        Retrieves tick data from the core server for the specified tick value.
+
+        Args:
+            tick (Optional[int]): The tick value for which data is to be retrieved. Must be a positive integer.
+                                If not provided, an exception is raised.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the tick data. If no data is available, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If the provided tick value is invalid or not provided, 
+                            or if there is an issue with the API request (e.g., network error, invalid response, or timeout).
+        """
 
         if not tick:
             raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_TICK_ERROR)
@@ -69,6 +109,16 @@ class QubiPy_Core:
     
     def get_tick_info(self) -> Dict[str, Any]:
 
+        """
+        Retrieves general tick information from the core server.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the tick information. If no data is available, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If there is an issue with the API request, such as a network error, invalid response, or timeout.
+        """
+
         try:
             response = requests.get(f'{self.core_url}{TICK_INFO}', headers=HEADERS, timeout=self.timeout)
             response.raise_for_status()  # Raise an exception for bad HTTP status codes
@@ -78,6 +128,22 @@ class QubiPy_Core:
             raise QubiPy_Exceptions(f'Error when getting tick info: {str(E)}') from None
     
     def get_tick_quorum_vote(self, tick: Optional[int] = None) -> Dict[str, Any]:
+
+        """
+        Retrieves quorum vote data for a specific tick from the core server.
+
+        Args:
+            tick (Optional[int]): The tick value for which quorum vote data is to be retrieved. Must be a positive integer.
+                                If not provided, an exception is raised.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the quorum vote data for the specified tick. 
+                            If no data is available, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If the provided tick value is invalid or not provided, 
+                            or if there is an issue with the API request (e.g., network error, invalid response, or timeout).
+        """
 
         if not tick:
             raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_TICK_ERROR)
@@ -96,6 +162,22 @@ class QubiPy_Core:
     
     def get_tick_transactions(self, tick: Optional[int] = None) -> Dict[str, Any]:
 
+        """
+        Retrieves transaction data for a specific tick from the core server.
+
+        Args:
+            tick (Optional[int]): The tick value for which transaction data is to be retrieved. Must be a positive integer.
+                                If not provided, an exception is raised.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the transactions for the specified tick. 
+                            If no transactions are found, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If the provided tick value is invalid or not provided, 
+                            or if there is an issue with the API request (e.g., network error, invalid response, or timeout).
+        """
+
         if not tick:
             raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_TICK_ERROR)
         
@@ -112,6 +194,22 @@ class QubiPy_Core:
             raise QubiPy_Exceptions(f'Error when getting tick transactions: {str(E)}') from None
         
     def get_tick_transactions_status(self, tick: Optional[int] = None) -> Dict[str, Any]:
+
+        """
+        Retrieves the status of transactions for a specific tick from the core server.
+
+        Args:
+            tick (Optional[int]): The tick value for which the transaction status is to be retrieved. Must be a positive integer.
+                                If not provided, an exception is raised.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the status of transactions for the specified tick. 
+                            If no status data is available, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If the provided tick value is invalid or not provided, 
+                            or if there is an issue with the API request (e.g., network error, invalid response, or timeout).
+        """
 
         if not tick:
             raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_TICK_ERROR)
@@ -132,6 +230,17 @@ class QubiPy_Core:
 
     def get_active_bets(self) -> Dict[str, Any]:
 
+        """
+        Retrieves the list of active bets from the core server.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the active bets data. 
+                            If no data is available, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If there is an issue with the API request, such as a network error, invalid response, or timeout.
+        """
+
         try:
             response = requests.get(f'{self.core_url}{ACTIVE_BETS}', headers=HEADERS, timeout=self.timeout)
             response.raise_for_status()  # Raise an exception for bad HTTP status codes
@@ -141,6 +250,22 @@ class QubiPy_Core:
             raise QubiPy_Exceptions(f'Error when getting active bets: {str(E)}') from None
     
     def get_active_bets_by_creator(self, creator_id: Optional[str] = None) -> Dict[str, Any]:
+
+        """
+        Retrieves the list of active bets created by a specific creator from the core server.
+
+        Args:
+            creator_id (Optional[str]): The ID of the creator whose active bets are to be retrieved. 
+                                        Must be a valid string. If not provided, an exception is raised.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the active bets created by the specified creator. 
+                            If no data is available, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If the provided creator ID is invalid or not provided, 
+                            or if there is an issue with the API request (e.g., network error, invalid response, or timeout).
+        """
 
         if not creator_id:
             raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_ADDRESS_ID)
@@ -160,6 +285,17 @@ class QubiPy_Core:
     
     def get_basic_info(self) -> Dict[str, Any]:
 
+        """
+        Retrieves basic information from the core server.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the basic information data.
+                            If no data is available, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If there is an issue with the API request, such as a network error, invalid response, or timeout.
+        """
+
         try:
             response = requests.get(f'{self.core_url}{BASIC_INFO}', headers=HEADERS, timeout=self.timeout)
             response.raise_for_status()  # Raise an exception for bad HTTP status codes
@@ -169,6 +305,22 @@ class QubiPy_Core:
             raise QubiPy_Exceptions(f'Error when getting computors: {str(E)}') from None
     
     def get_bet_info(self, bet_id: Optional[int] = None) -> Dict[str, Any]:
+
+        """
+        Retrieves information about a specific bet using its ID from the core server.
+
+        Args:
+            bet_id (Optional[int]): The ID of the bet to retrieve information for. 
+                                    Must be a positive integer. If not provided, an exception is raised.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the information for the specified bet. 
+                            If no data is available, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If the provided bet ID is invalid or not provided, 
+                            or if there is an issue with the API request (e.g., network error, invalid response, or timeout).
+        """
 
         if not bet_id:
             raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_BET_ID)
@@ -187,6 +339,24 @@ class QubiPy_Core:
             raise QubiPy_Exceptions(f'Error when getting bet info by id: {str(E)}') from None
         
     def get_bettors_by_bet_options(self, bet_id: Optional[int] = None, bet_option: Optional[int] = None) -> Dict[str, Any]:
+
+        """
+        Retrieves a list of bettors for a specific bet and bet option from the core server.
+
+        Args:
+            bet_id (Optional[int]): The ID of the bet to retrieve bettors for. Must be a positive integer.
+                                    If not provided, an exception is raised.
+            bet_option (Optional[int]): The option of the bet for which to retrieve bettors. 
+                                        Must be a positive integer. If not provided, an exception is raised.
+
+        Returns:
+            Dict[str, Any]: A dictionary containing the list of bettors for the specified bet and bet option. 
+                            If no data is available, an empty dictionary is returned.
+
+        Raises:
+            QubiPy_Exceptions: If the provided bet ID or bet option is invalid or not provided, 
+                            or if there is an issue with the API request (e.g., network error, invalid response, or timeout).
+        """
 
         if not bet_id or not bet_option:
             raise QubiPy_Exceptions(QubiPy_Exceptions.INVALID_BET_OPTIONS)
