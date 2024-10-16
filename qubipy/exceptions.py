700fa63b8919986091718160d8f7e277c559417b
Mazzya <yahyamazouarenna@gmail.com>
2024-10-16 16:37:37 +0200

    Update exceptions.py

--- qubipy/exceptions.py
+++ qubipy/exceptions.py
@@ -27,3 +27,5 @@ class QubiPy_Exceptions(Exception):
     INVALID_DATA_FORMAT = 'Invalid data format detected, please try again'
 
     INVALID_BET_ID = 'Invalid bet ID, try again'
+
+    INVALID_BET_OPTIONS = 'Invalid bet options and bet ID'
