ada609e8bfcce6a353095a930de653b5987c765c
RealWorld777 <valkyrie.prim@gmail.com>
2024-10-23 15:17:30 +0900

    refactor: file structure

--- qubipy/tx/utils.py
+++ qubipy/tx/utils.py
@@ -1,84 +1,12 @@
-"""
-utils.py
-Utility functions for the Qubic Transaction
-"""
-from qubipy.crypto.utils import sign, verify, kangaroo_twelve, get_private_key_from_subseed, get_subseed_from_seed, get_public_key_from_private_key, get_identity_from_public_key
+from qubipy.tx.builder import Tx_Builder
 
-class QubiPy_Tx:
-    def __init__(self):
-        self.built_data = bytearray()
-
-        self.source_public_key = None
-        self.destination_public_key = None
-        self.amount = 0
-        self.target_tick = 0
-        self.input_type = 0
-        self.input_size = 0
-
-        self.payload = None
-
-    def set_source_public_key(self, public_key: bytes) -> 'QubiPy_Tx':
-        self.source_public_key = public_key
-        return self
+def create_tx(seed: str, source_public_key: bytes, destination_public_key: bytes, amount: int, target_tick: int, input_type: int, input_size: int) -> tuple[bytes, bytes, bytes, bytes]:
+    tx = Tx_Builder()
+    tx.set_source_public_key(source_public_key)
+    tx.set_destination_public_key(destination_public_key)
+    tx.set_amount(amount)
+    tx.set_target_tick(target_tick)
+    tx.set_input_type(input_type)
+    tx.set_input_size(input_size)
     
-    def set_destination_public_key(self, public_key: bytes) -> 'QubiPy_Tx':
-        self.destination_public_key = public_key
-        return self
-    
-    def set_amount(self, amount: int) -> 'QubiPy_Tx':
-        self.amount = amount
-        return self
-    
-    def set_target_tick(self, tick: int) -> 'QubiPy_Tx':
-        self.target_tick = tick
-        return self
-    
-    def set_input_type(self, input_type: int) -> 'QubiPy_Tx':
-        self.input_type = input_type
-        return self
-    
-    def set_input_size(self, input_size: int) -> 'QubiPy_Tx':
-        self.input_size = input_size
-        return self
-    
-    def build(self, seed: str) -> bytes:
-        offset = 0
-
-        if self.source_public_key:
-            self.built_data[offset:offset+len(self.source_public_key)] = self.source_public_key
-            offset += len(self.source_public_key)
-        
-        if self.destination_public_key:
-            self.built_data[offset:offset+len(self.destination_public_key)] = self.destination_public_key
-            offset += len(self.destination_public_key)
-        
-        self.built_data[offset:offset+8] = self.amount.to_bytes(8, byteorder='little')
-        offset += 8
-        
-        self.built_data[offset:offset+4] = self.target_tick.to_bytes(4, byteorder='little')
-        offset += 4
-        
-        self.built_data[offset:offset+2] = self.input_type.to_bytes(2, byteorder='little')
-        offset += 2
-        
-        self.built_data[offset:offset+2] = self.input_size.to_bytes(2, byteorder='little')
-        offset += 2
-        
-        # Sign the transaction
-        seed_bytes = bytes(seed, 'utf-8')
-
-        subseed = get_subseed_from_seed(seed_bytes)
-        private_key = get_private_key_from_subseed(subseed)
-        public_key = get_public_key_from_private_key(private_key)
-
-        tx_digest = kangaroo_twelve(self.built_data, offset, 32)
-
-        signature = sign(subseed, public_key, tx_digest)
-
-        self.built_data[offset:offset+len(signature)] = signature
-        offset += len(signature)
-
-        digest = kangaroo_twelve(self.built_data, offset, 32)
-        tx_hash = get_identity_from_public_key(digest).lower()
-
-        return self.built_data[:80], self.built_data, signature, tx_hash
+    return tx.build(seed)
