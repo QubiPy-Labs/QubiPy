e2bdfbb1fcd948487649fe555670605f04e4593f
RealWorld777 <valkyrie.prim@gmail.com>
2024-10-23 09:50:47 +0900

    feat: add build and sign

--- qubipy/tx/utils.py
+++ qubipy/tx/utils.py
@@ -2,7 +2,7 @@
 utils.py
 Utility functions for the Qubic Transaction
 """
-from qubipy.crypto.utils import sign, verify, getTxHashFromDigest, getPrivateKeyFromSubSeed
+from qubipy.crypto.utils import sign, verify, kangaroo_twelve, get_private_key_from_subseed, get_subseed_from_seed, get_public_key_from_private_key, get_identity_from_public_key
 
 class QubiPy_Tx:
     def __init__(self):
@@ -42,39 +42,43 @@ class QubiPy_Tx:
         return self
     
     def build(self, seed: str) -> bytes:
-        self.built_data = bytearray([0])
         offset = 0
 
-        # Serialize source_public_key
         if self.source_public_key:
             self.built_data[offset:offset+len(self.source_public_key)] = self.source_public_key
             offset += len(self.source_public_key)
         
-        # Serialize destination_public_key
         if self.destination_public_key:
             self.built_data[offset:offset+len(self.destination_public_key)] = self.destination_public_key
             offset += len(self.destination_public_key)
         
-        # Serialize amount as 8 bytes big endian
-        self.built_data[offset:offset+8] = self.amount.to_bytes(8, byteorder='big')
+        self.built_data[offset:offset+8] = self.amount.to_bytes(8, byteorder='little')
         offset += 8
         
-        # Serialize target_tick as 4 bytes big endian
-        self.built_data[offset:offset+4] = self.target_tick.to_bytes(4, byteorder='big')
+        self.built_data[offset:offset+4] = self.target_tick.to_bytes(4, byteorder='little')
         offset += 4
         
-        # Serialize input_type as 2 bytes big endian
-        self.built_data[offset:offset+2] = self.input_type.to_bytes(2, byteorder='big')
+        self.built_data[offset:offset+2] = self.input_type.to_bytes(2, byteorder='little')
         offset += 2
         
-        # Serialize input_size as 2 bytes big endian
-        self.built_data[offset:offset+2] = self.input_size.to_bytes(2, byteorder='big')
+        self.built_data[offset:offset+2] = self.input_size.to_bytes(2, byteorder='little')
         offset += 2
         
-        # Serialize seed
-        seed_bytes = seed.encode('utf-8')
-        
-        private_key = get_private_key_from_seed(seed)
-        signature = sign(private_key, self.built_data)
-        
-        return self.built_data
+        # Sign the transaction
+        seed_bytes = bytes(seed, 'utf-8')
+
+        subseed = get_subseed_from_seed(seed_bytes)
+        private_key = get_private_key_from_subseed(subseed)
+        public_key = get_public_key_from_private_key(private_key)
+
+        tx_digest = kangaroo_twelve(self.built_data, offset, 32)
+
+        signature = sign(subseed, public_key, tx_digest)
+
+        self.built_data[offset:offset+len(signature)] = signature
+        offset += len(signature)
+
+        digest = kangaroo_twelve(self.built_data, offset, 32)
+        tx_hash = get_identity_from_public_key(digest).lower()
+
+        return self.built_data[:80], self.built_data, signature, tx_hash
